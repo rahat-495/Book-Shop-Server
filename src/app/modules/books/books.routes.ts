79d4efe070a7b3e9d0c088dd039703f0138665bb
rahat-495 <md8702687@gmail.com>
2025-04-08 11:50:29 +0600

    Rahat : book routes user validations are done !

--- src/app/modules/books/books.routes.ts
+++ src/app/modules/books/books.routes.ts
@@ -5,13 +5,14 @@ import { bookValidations } from "./books.validations";
 import { upload } from "../../utils/sendImageToCloudinary";
 import { parseTextDataToJsonData } from "./books.utils";
 import validateRequest from "../../../middlewares/validateRequest";
+import auth from "../../../middlewares/auth";
 
 const router = Router() ;
 
 router.get('/' , bookControllers.getAllBooks)
-router.delete('/:id' , bookControllers.removeBook)
+router.delete('/:id' , auth("admin") , bookControllers.removeBook)
 router.get('/get-single-book/:id' , bookControllers.getSingleBook)
-router.post('/create-book' , upload.single("file") , parseTextDataToJsonData , validateRequest(bookValidations.createBookValidationSchema) , bookControllers.createBook) ;
-router.put('/update-book/:id' , upload.single("file") , parseTextDataToJsonData , validateRequest(bookValidations.updateBookValidationSchema) , bookControllers.updateBook) ;
+router.post('/create-book' , upload.single("file") , parseTextDataToJsonData , validateRequest(bookValidations.createBookValidationSchema) , auth("admin") , bookControllers.createBook) ;
+router.put('/update-book/:id' , upload.single("file") , parseTextDataToJsonData , validateRequest(bookValidations.updateBookValidationSchema) , auth("admin") , bookControllers.updateBook) ;
 
 export const booksRoutes = router ;
